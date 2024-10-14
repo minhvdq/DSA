bcec12aa83ffc5e52f2d9c41605403e03dd8b17a
Damian@Vu <minhvdq@gmail.com>
2024-10-14 14:21:45 -0400

    632 241

---
+++ Leetcode/Solution_632.java
@@ -0,0 +1,41 @@
+package Leetcode;
+import java.util.*;
+
+/**
+ * Leetcode 632. Smallest Range Covering Elements from K Lists
+ */
+public class Solution {
+    int[] indices;
+    int minIndex = 0;
+    int[] range = {0, Integer.MAX_VALUE};
+    public int[] smallestRange(List<List<Integer>> nums) {
+        int leng = nums.size();
+        indices = new int[leng];
+
+        while(true){
+            int inf = Integer.MAX_VALUE, sup = -Integer.MAX_VALUE, minInd = 0;
+            for( int i = 0; i < leng; i ++ ) {
+                int num = nums.get(i).get(indices[i]);
+
+                if( num < inf ){
+                    inf = num;
+                    minInd = i;
+                }
+
+                if( num > sup ) {
+                    sup = num;
+                }
+            }
+            if( sup - inf < range[1] - range[0] ){
+                range[0] = inf;
+                range[1] = sup;
+            }
+
+            indices[minInd] ++;
+            if( indices[minInd] == nums.get(minInd).size() ){
+                break;
+            }
+        }
+        return range;
+    }
+}
